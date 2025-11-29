224b8c346a0c9dbdb7c368e4b10bfb3b883c4c38
JMoney3336 <jcalhoun3424@gmail.com>
2025-11-29 09:04:36 -0600

    Update Mole.java

--- src/application/model/Mole.java
+++ src/application/model/Mole.java
@@ -11,13 +11,49 @@ public class Mole implements Runnable {
 	private Image moleImage;
 	private int index;
 	private Random rand;
+	private long exposedStartTime;
 	
-	public Mole(WhackAMole game, MainView mainview) {
-		moleImage = new Image(getClass().getResourceAsStream("mole.png"));
-		// TODO
+	public Mole(WhackAMole game, MainView mainview, int index, Random rand) {
+		this.game = game;
+		this.mainView = mainview;
+		this.moleImage = new Image(getClass().getResourceAsStream("/mole.png"));
+		this.index = index;
+		this.rand = rand;
 	}
 	
-	public synchronized void run() {
-        // TODO
-    }
+	public void run() {
+		while (!game.gameOver()) {
+			game.setExposed(index, false);
+			mainView.displayImage(index, null);
+			
+			int hiddenTime = rand.nextInt(3001) + 2000;
+			
+			try {
+				Thread.sleep(hiddenTime);
+			} catch (InterruptedException e) {
+				if (game.gameOver()) {
+					return;
+				}
+			}
+			
+			mainView.displayImage(index,moleImage);
+			game.setExposed(index, true);
+			
+			exposedStartTime = System.currentTimeMillis();
+			int showTime = rand.nextInt(1001) + 1000;
+			
+			try {
+				Thread.sleep(showTime);
+			} catch (InterruptedException e) {
+				if (game.gameOver()) {
+					return;
+				}
+				long elapsed = System.currentTimeMillis() - exposedStartTime;
+				game.updateScore(elapsed);
+				}
+			
+			mainView.displayImage(index, null);
+			game.setExposed(index, false);
+		}
+	}
 }
